552e6cb7fe146e0398231fce4a9ee17d609f2ef4
Kurt Riecken <kurt.riecken@gmail.com>
2023-12-01 18:54:08 -0500

    changed to react app; removed all starter code from vite; added header and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <>
+            <div>
+                <nav>
+                    <a href="https://www.google.com">Google</a>
+                    <a href="https://www.github.com">GitHub</a>
+                    <a href="https://www.linkedin.com">LinkedIn</a>
+                </nav>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <>
+            <div>
+                <nav>
+                    <a href="https://www.google.com">Google</a>
+                    <a href="https://www.github.com">GitHub</a>
+                    <a href="https://www.linkedin.com">LinkedIn</a>
+                </nav>
+            </div>
+        </>
+    )
+}
