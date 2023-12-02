7f39f19f680278a9aa1b93c7d61f72e422f305a8
Kurt Riecken <kurt.riecken@gmail.com>
2023-12-01 20:34:17 -0500

    added styling and flexbox to header and footer; brought in icons for socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,21 @@
+import gitHubLogo from '../assets/github-mark.png'
+import linkedInLogo from '../assets/linkedin.svg'
 import './Footer.css'
 
 export default function Footer() {
     return (
         <>
-            <div>
-                <nav>
-                    <a href="https://www.google.com">Google</a>
-                    <a href="https://www.github.com">GitHub</a>
-                    <a href="https://www.linkedin.com">LinkedIn</a>
+            <div className='footer'>
+                <nav className='socials_links'>
+                    <a href="https://www.google.com">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" alt="" />
+                    </a>
+                    <a href="https://www.github.com">
+                        <img src={gitHubLogo} alt="" />
+                    </a>
+                    <a href="https://www.linkedin.com">
+                        <img src={linkedInLogo} alt="" />
+                    </a>
                 </nav>
             </div>
         </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
+import kurtPicture from '../assets/Kurt_Riecken.jpg'
 import './Header.css'
 
 export default function Header() {
     return (
         <div className='header'>
             <div className="kurt_picture">
-                <a href="/"></a>
+                <a href="#">
+                    <img src={kurtPicture} alt="" />
+                </a>
             </div>
             <div className='navbar'>
                 <nav>
