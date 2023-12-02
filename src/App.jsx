251578524a8885144d516725d31029795c031a85
Kurt Riecken <kurt.riecken@gmail.com>
2023-12-02 14:02:30 -0500

    added PrimeReact component library

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+
+import "primereact/resources/themes/mdc-dark-deeppurple/theme.css";
+
+
+import { Button } from 'primereact/button';  
+        
 
 function App() {
 
   return (
-    <>
+    <PrimeReactProvider>
+      <Button label="Submit"/>
       <Header />
       <h1>Hello! This is my new portfolio</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facere maxime libero repudiandae placeat ut perferendis veritatis ex cum nam, voluptatibus alias delectus laboriosam neque laudantium aliquam accusamus soluta quo!
@@ -14,7 +22,7 @@ function App() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores necessitatibus assumenda sed architecto eveniet voluptatum natus. Molestiae, quae. Rerum ducimus modi fugit earum exercitationem corporis quidem labore cum in quo?
       </p>
       <Footer />
-    </>
+    </PrimeReactProvider>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,16 @@
 import gitHubLogo from '../assets/github-mark.png'
 import linkedInLogo from '../assets/linkedin.svg'
 import './Footer.css'
+// import { EmailIcon, PhoneIcon } from '@chakra-ui/icons'
+import { Button } from 'primereact/button';
+
 
 export default function Footer() {
     return (
         <>
             <div className='footer'>
                 <nav className='socials_links'>
+                    <Button label="Submit" />
                     <a href="https://www.google.com">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" alt="" />
                     </a>
@@ -16,6 +20,12 @@ export default function Footer() {
                     <a href="https://www.linkedin.com">
                         <img src={linkedInLogo} alt="" />
                     </a>
+                    {/* <a href="https://www.linkedin.com">
+                        <EmailIcon />
+                    </a>
+                    <a href="https://www.linkedin.com">
+                        <PhoneIcon />
+                    </a> */}
                 </nav>
             </div>
         </>
