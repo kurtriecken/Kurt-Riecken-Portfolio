9bc645d4ee7c750832d62d237ea945d42c66f5a6
Kurt Riecken <kurt.riecken@gmail.com>
2023-12-05 07:10:22 -0500

    reorganized footer; removed old files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,6 @@ import gitHubLogo from '../assets/github-mark.png'
 import linkedInLogo from '../assets/linkedin.svg'
 import reactLogo from '../assets/react.svg'
 import './Footer.css'
-// import { EmailIcon, PhoneIcon } from '@chakra-ui/icons'
-import { Button } from 'primereact/button';
 import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -16,28 +14,17 @@ export default function Footer() {
             <Link className='mb-3' to="/"><FontAwesomeIcon className='m-auto' icon={faHouseChimneyWindow} style={{ width: "35px", height: 'auto' }} /></Link>
 
             <div className='footer container m-auto card border-0 bg-transparent'>
-                <div className='row flex text-center my-1'>
-                    <p className='my-0'>Built with <span><img className='reactLogo' src={reactLogo} alt="" /></span></p>
-                </div>
                 <nav className='socials_links my-1 container flex text-center'>
-                    {/* <Button label="Submit" /> */}
-                    {/* <h3 className='col-3'>Socials: </h3> */}
-                    {/* <a className='mx-1' href="mailto:kurt.riecken@gmail.com">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" alt="" />
-                    </a> */}
                     <a className='mx-1' href="https://www.github.com/kurtriecken">
                         <img src={gitHubLogo} alt="" />
                     </a>
                     <a className='mx-1' href="https://www.linkedin.com/in/kurt-riecken-95ba77158/">
                         <img src={linkedInLogo} alt="" />
                     </a>
-                    {/* <a href="https://www.linkedin.com">
-                        <EmailIcon />
-                    </a>
-                    <a href="https://www.linkedin.com">
-                        <PhoneIcon />
-                    </a> */}
                 </nav>
+                <div className='row flex text-center my-1'>
+                    <p className='my-0'>Built with <span><img className='reactLogo' src={reactLogo} alt="" /></span></p>
+                </div>
             </div>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
 import kurtPicture from '../assets/Kurt_Riecken.jpg'
 import './Header.css'
 import React, { useState } from 'react';
-import { Button } from 'primereact/button';
 import { TabMenu } from 'primereact/tabmenu';
-import { Link } from 'react-router-dom';
 
 
 export default function Header() {
     const [activeIndex, setActiveIndex] = useState(3);
     const items = [
-        // {label: 'Home', icon: 'pi pi-fw pi-home'},
         { label: 'About me', icon: 'pi pi-fw pi-calendar', url: 'aboutme' },
         { label: 'Portfolio', icon: 'pi pi-fw pi-pencil', url: 'portfolio' },
         { label: 'Resume', icon: 'pi pi-fw pi-file', url: 'resume' },
@@ -19,34 +16,14 @@ export default function Header() {
     return (
         <div className='header'>
             <div className="kurt_picture">
-                {/* <Link to="/">← Go Back</Link> */}
-                {/* <a href="#"> */}
                 <img src={kurtPicture} alt="" />
-                {/* </a> */}
             </div>
             <div className="card bg-transparent">
-                {/* <Button onClick={() => setActiveIndex(0)} className="p-button-outlined mb-5" label="Activate 1st" /> */}
                 <TabMenu to='/' className='p-menuitem-link bg-transparent' model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} />
             </div>
-            {/* <div className='navbar'>
-                <nav>
-                    <a href="#">About me</a>
-                    <a href="#">Portfolio</a>
-                    <a href="#">Resume</a>
-                    <a href="#">Contact</a>
-                </nav>
-            </div> */}
         </div>
     )
 }
 
 
 
-
-// export default function ControlledDemo() {
-
-
-//     return (
-
-//     )
-// }
